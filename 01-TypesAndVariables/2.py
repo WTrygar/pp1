3335a2095237aa01c653f4c22638638589649a98
WTrygar <56258624+WTrygar@users.noreply.github.com>
2019-10-14 14:51:55 +0200

    Add files via upload

---
+++ 01-TypesAndVariables/2.py
@@ -0,0 +1,8 @@
+x=7
+y=34
+print(x)
+print(y)
+z=x
+x=y
+y=z
+print(z)
---
+++ 01-TypesAndVariables/5.py
@@ -0,0 +1,4 @@
+imie = input("podaj imie")
+nazwisko = input("podaj nazwisko")
+print(str.format("imie to {}, a nazwisko to {}")
+    
